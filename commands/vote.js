68c0b81761ec21ccd1415cbdc648b2d78fa6d5aa
Dacrenzio <daniele.dominici18@gmail.com>
2020-11-21 02:15:58 +0100

    corretto ordine di errore

--- commands/vote.js
+++ commands/vote.js
@@ -7,14 +7,14 @@ module.exports = {
 		let err = require("../functions/errors");
 		const unMute = require('./unmuteall.js');
 
-		if(err.errors([0, 1, 2, 3, 4, 5, 8], moderatore, message))return;
-
 
 		if(message.channel.name.valueOf() !== message.member.user.id.valueOf()){
 			message.delete();
 			embed.sendEmbed([255, 0, 0], "Hai mandato il voto nella chat sbagliata!", message.channel);
 			return;
 		}
+
+		if(err.errors([0, 1, 2, 3, 4, 5, 8], moderatore, message))return;
 
 		let mentioned = message.mentions.members.first();
 		if(moderatore.ballottaggio.length > 0 && !moderatore.ballottaggio.includes(mentioned)){
